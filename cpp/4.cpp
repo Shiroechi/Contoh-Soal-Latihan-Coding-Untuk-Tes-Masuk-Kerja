4ed44d3fb3793c72712c5b9b7e19d3860ba940c3
MadeYoga <vngla21@gmail.com>
2021-10-07 14:24:13 +0700

    Update C++ solution 4

--- cpp/4.cpp
+++ cpp/4.cpp
@@ -1,117 +1,151 @@
-#include<stdio.h>
-#include<math.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <time.h>
 
-void main ()
+void print_array(std::vector<int> &array)
 {
-	int data[10], n, m, ganjil, genap;
-	bool a=true;
-
-	ganjil = 0; genap = 0;
-
-	while (true)
+	for (int i = 0; i < array.size(); i++)
 	{
-		printf("masukan n :");
-		scanf("%d", &n);
-		
-		printf("masukan m :");
-		scanf("%d", &m);
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
-		if (n<=m)
+
+void fill_array(std::vector<int> &array)
+{
+	for (int i = 0; i < array.size(); i++)
+	{
+		array[i] = i;
+	}
+
+	std::random_shuffle(array.begin(), array.end());
+
+	std::stack<int> odd_stack;
+	std::stack<int> even_stack;
+	for (int i = 0; i < array.size(); i++)
+	{
+		int number = array[i];
+		if (number % 2 == 0)
 		{
-			break;
+			even_stack.push(number);
 		}
+		else
+		{
+			odd_stack.push(number);
+		}
+	}
+
+	for (int i = 0; i < array.size(); i++)
+	{
+		if (i % 2 == 0)
+		{
+			array[i] = even_stack.top();
+			even_stack.pop();
+		}
+		else
+		{
+			array[i] = odd_stack.top();
+			odd_stack.pop();
+		}
+	}
+}
+
+
+bool sequential_search(std::vector<int> array1, int number)
+{
+	for (int item : array1)
+	{
+		if (item == number)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+
+std::vector<int> union_array(std::vector<int> array1, std::vector<int> array2)
+{
+	int new_size = array1.size() + array2.size();
+	std::vector<int> temp(new_size);
+	
+	for (int i = 0; i < array1.size(); i++)
+	{
+		temp[i] = array1[i];
+	}
+
+	int offset = array1.size();
+
+	for (int i = 0; i < array2.size(); i++)
+	{
+		temp[offset + i] = array2[i];
 	}
 	
-	srand (time(NULL));
+	return temp;
+}
 
-	for (int i = 0 ; i < n ; i++)
+
+std::vector<int> intersection_array(std::vector<int> array1, std::vector<int> array2)
+{
+	std::vector<int> temp;
+
+	for (int number : array1)
 	{
-		if (i==0)
+		if (sequential_search(array2, number) == true)
 		{
-			data[i] = rand () % m ;
-
-			if (data[i] % 2 == 0)
-			{
-				genap++;
-			}
-			else if (data[i] % 2 == 1)
-			{
-				ganjil++;
-			}
+			temp.push_back(number);
 		}
-
-		if (ganjil > 0)
-		{
-			data[i] = rand () % m;
-			if (data[i] % 2 == 1)
-			{
-				ganjil--;
-				genap++;
-			}
-			else 
-			{
-				while (data[i] % 2 == 1)
-				{
-					data[i] = rand () % m;
-
-					if (data[i] % 2 == 1)
-					{
-					ganjil--;
-					genap++;
-					}
-				}
-			}
-		}
-		else if (genap > 0)
-		{
-			data [i] = rand () % m;
-			if (data[i] % 2 == 0 )
-			{
-				genap--;
-				ganjil++;
-			}
-			else 
-			{
-				while(data[i] % 2 == 0)
-				{
-					data[i] = rand() % m;
-
-					if (data[i] % 2 == 0 )
-					{
-						genap--;
-						ganjil++;
-					}
-				}
-			}
-		}
-		//cek kembar atau tidak 
-		/*bool kmbr = false;
-		do {
-			data[i] = rand() % m;
-			kmbr = false;
-			for (int j = 0; j < i; j++)
-			{
-				if (data[i] == data[j])
-				{
-					kmbr = true;
-				}
-				
-			}
-
-		} while (kmbr || genapganjiltidakcocok);*/
 	}
 
-	//menampilkan data
-	for(int i = 0 ; i < n ; i++)
+	return temp;
+}
+
+
+std::vector<int> subtract_array(std::vector<int> array1, std::vector<int> array2)
+{
+	std::vector<int> temp = array1;
+
+	for (int i = 0; i < array2.size(); i++)
 	{
-		printf("%d", data[i]);
-		printf(" ");
+        int number = array2[i];
+        temp.erase(std::find(temp.begin(), temp.end(), number));
 	}
 
+	return temp;
+}
 
 
-	getch();
+int main()
+{
+	int m;
+
+	std::cout << "m: ";
+	std::cin >> m;
+
+	std::srand(time(0));
+
+	std::vector<int> array1(m);
+	fill_array(array1);
+	print_array(array1);
+
+	std::vector<int> array2(m);
+	fill_array(array2);
+	print_array(array2);
+
+	std::cout << "\nUnion:\n";
+	std::vector<int> array3 = union_array(array1, array2);
+	print_array(array3);
+
+	std::cout << "\nIntersection:\n";
+	std::vector<int> array4 = intersection_array(array1, array2);
+	print_array(array4);
+
+	std::cout << "\nUnion subtract intersection:\n";
+	std::vector<int> array5 = subtract_array(array3, array4);
+	print_array(array5);
+
+	return 0;
 }
